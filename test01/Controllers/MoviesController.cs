07e9d4b5a09399e0f858b1304dfa8bbc9546141a
saramohamed512 <saramohamed12656@gmail.com>
2025-09-29 20:13:15 +0300

    Session02: Action Parameters

--- test01/Controllers/MoviesController.cs
+++ test01/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ namespace test01.Controllers
 {
     public class MoviesController : Controller
     {
+        #region Action Types
         //Must Be Public Non Static Method => action
         //Action => Return Data To Web App
         //public ContentResult Index()
@@ -17,15 +18,30 @@ namespace test01.Controllers
         //    var Result = new RedirectResult("https://www.google.com");
         //    return Result;
         //}
-        public IActionResult Index()
-        {
-            return Content("Batman");
-        }
+        //public IActionResult Index()
+        //{
+        //    return Content("Batman");
+        //}
 
-        public IActionResult Redirect()
-        {
-            return RedirectToAction("Index");
+        //public IActionResult Redirect()
+        //{
+        //    return RedirectToAction("Index");
+        //}
+
+        #endregion
+        #region Action Parameter
+        public IActionResult Index() 
+        { 
+            return Content("Hello From Movies"); 
         }
+        public IActionResult GetMovie(int id , string name)
+        {
+            //1. Send the data from form
+            //2. Send the data via Sigment of URL
+            //3. Send the data via Query
+            return Content($"Movie :  {id} | Name : {name}");
+        }
+        #endregion
 
     }
 }
